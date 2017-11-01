380b988b964bf6252a82b2d0035303778306848f
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-01 17:02:30 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -19,7 +19,7 @@ class PigLatinizer
     end
   end
 
-  def to_pig_latin(@text)
-    @text.split.map {|word| piglatinize(word)}.join(" ")
+  def to_pig_latin(sentence)
+    sentence.split.map {|word| piglatinize(word)}.join(" ")
   end
 end
