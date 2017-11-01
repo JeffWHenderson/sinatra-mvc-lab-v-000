2d11adafe271ec52f504742efc296109e6a3c5a5
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-01 16:29:34 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,10 +7,12 @@ class PigLatinizer
 
   def piglatinize(word)
     vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    consonants = []
+    
     if vowels.include?(word[0])
       word + 'way'
     else
-      word[0] << consonants 
+      word[0] << consonants
       word + consonants + "ay"
     end
   end
