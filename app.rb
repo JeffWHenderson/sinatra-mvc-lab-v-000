475d7fefeb89b17e021c38eefaeb0789fe00615f
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-01 16:12:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   post '/piggy' do
     #text_from_user = params[:user_text]
     #@a = PigLatinize.new(params[:user_phrase])
-    @a = 'hello world'
+    @text = 'hello world'
     erb :piggy
   end
 end
