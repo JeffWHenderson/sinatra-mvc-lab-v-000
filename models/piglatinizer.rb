3de807d4f37c63a989d93b5eb8cfcae3e47e4716
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-01 17:03:06 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,9 +1,9 @@
 class PigLatinizer
   attr_reader :text
 
-  def intialize(text)
-    @text = text
-  end
+  # def intialize(text)
+  #   @text = text
+  # end
 
   def piglatinize(word)
     vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
